c79ac9ff2b33ca458236b2243ecc1fdcafddfef5
Ryuuu <ryumi.zenyx@gmail.com>
2026-02-27 14:51:54 +0700

    update api

--- api.php
+++ api.php
@@ -8,9 +8,9 @@ mysqli_report(MYSQLI_REPORT_OFF);
 
 // === KONFIGURASI DATABASE ===
 $servername = "localhost";
-$username   = "underwat_daffa_water";
-$password   = "DDKg4Br3WwYBeTkyg7Dm";
-$dbname     = "underwat_daffa_water";
+$username   = "underwat_aquatic";
+$password   = "YN3xpx2hQRHNbWshgpVT";
+$dbname     = "underwat_aquatic";
 
 // === KONEKSI DATABASE ===
 $conn = new mysqli($servername, $username, $password, $dbname);
